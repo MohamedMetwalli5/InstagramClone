e6e2229e2c8c43d1d7237c490a494ab56b2ba1af
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-08-18 21:31:01 +0200

    third commit Message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,15 +8,18 @@
             <img src="/svg/InstagramLogo.svg" alt="" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>Mohamed Metwalli</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="https://www.linkedin.com/in/mohamed-metwalli5/">Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>22k</strong> followers</div>
                 <div class="pr-5"><strong>207</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">https://mohamedmetwalli5.github.io/#/</div>
-            <div>fasdfsdfasdf  dsfafsdf sd fasfsdf s</div>
-            <div> <a href="https://www.linkedin.com/in/mohamed-metwalli5/">linkedin</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div> <a href="https://www.linkedin.com/in/mohamed-metwalli5/">{{ $user->profile->url }}</a></div>
         </div>
     </div>
     <div class="row pt-5">
