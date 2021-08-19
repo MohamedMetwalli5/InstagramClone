e709c87d28898c6004e8b1b145353fc75828b225
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-08-19 21:24:57 +0200

    posts are almost done

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Intervention\Image\Facades\Image;
 class PostsController extends Controller
 {
 
@@ -23,10 +23,22 @@ class PostsController extends Controller
         ]);
 
         // \App\Models\Post::create($data);
+        
+        $imagePath = request('image')->store('uploads','public');
+        
+        $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
+        $image->save();
 
-        auth()->user()->posts()->create($data);  
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $imagePath,
+        ]);  
 
-        dd(request()->all());
+        return redirect('/profile/'.auth()->user()->id); // going to our authentication user and grapping his/her id
+    }
+
+    public function show(\App\Models\Post $post){ // to show the post when we click on it in another page
+        return view('posts.show', compact('post'));
     }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,7 @@ Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create'])
 
 Route::post('/p', [App\Http\Controllers\PostsController::class, 'store']);
 
+Route::get('/p/{post}', [App\Http\Controllers\PostsController::class, 'show']);
+
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
  
