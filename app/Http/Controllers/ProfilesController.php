eebba08695957f98d3912b10a5f322c22865bac3
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-08-19 23:41:05 +0200

    editing the profile is almost done

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -7,11 +7,29 @@ use App\Models\User;
 
 class ProfilesController extends Controller
 {
-    public function index($user){
+    public function index(User $user){
         // dd($user);
-        $user = User::findOrFail($user);
-        return view('profiles.index', [
-            'user' => $user,
+        // $user = User::findOrFail($user);
+        return view('profiles.index', compact('user'));
+    }
+
+    public function edit(User $user){
+        return view('profiles.edit', compact('user'));
+    }
+
+    public function update(User $user){
+        $data = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'url',
+            'image' => '',
+
         ]);
+
+        // dd($data);
+        $user->profile->update($data);
+
+        return redirect("/profile/{$user->id}");
+
     }
 }
